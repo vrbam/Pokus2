f7c650b2d9cfce17b6e748650e0d085e0e0e8634
Mikuláš Vrba <vrbam.02@spst.eu>
2020-11-16 12:51:12 +0100

    Odčítání

--- Pokus2/Form1.cs
+++ Pokus2/Form1.cs
@@ -16,5 +16,21 @@ namespace Pokus2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            MessageBox.Show((a + b).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            MessageBox.Show((a - b).ToString());
+        }
     }
 }
